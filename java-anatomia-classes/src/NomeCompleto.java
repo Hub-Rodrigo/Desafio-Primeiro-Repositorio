49e2dbcd06097be75429aa00899299747f15805e
rodrigo <rodrigosf494@gmail.com>
2022-10-06 18:50:47 -0300

    Atribuindo nome completo a variável

---
+++ java-anatomia-classes/src/NomeCompleto.java
@@ -0,0 +1,18 @@
+public class NomeCompleto {
+
+public static void main (String[] args){
+    
+    //Atribuindo nome e sobremone a variavel
+    String primeiroNome = "Rodrigo";
+    String sengundoNome = "Souza";
+
+    //juntando nome, sobronome e imprimindo nome completo
+    String nomeCompleto = nomeCompleto(primeiroNome, sengundoNome);
+    System.out.println(nomeCompleto);
+} 
+//metodo para atribuir nome completo a variavel
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
+}
+    
+}
